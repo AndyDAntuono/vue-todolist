d9e46a63f65b51c5cbcd8f262d9db20ea61332cd
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-18 18:56:59 +0200

    [UPD] partially redone the html code; updated script.js

--- js/script.js
+++ js/script.js
@@ -11,8 +11,25 @@ createApp ({
                 {
                     text: "Fare la spesa",
                     done: false, 
+                },
+                {
+                    text: "Eseguire seppuku",
+                    done: false,
+                },
+                {
+                    text: "Far fuori il mio vicino di casa",
+                    done: true,
+                },
+                {
+                    text: "Effettuare lavanda gastrica",
+                    done: true,
+                },
+                {
+                    text: "Contattare l'ingenier Cane riguado il ponte sullo stetto di Messina",
+                    done: false,
                 }
             ]
         }
+        
     }
 }).mount('#app');
